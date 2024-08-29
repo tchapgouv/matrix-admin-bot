93bffd4fb12fdc3af891a88ce7eea732c29c2a2a
mcalinghee <mcalinghee.dev@gmail.com>
2024-08-29 17:18:33 +0200

    ignore if not on the same homeserver, send report when command is processed, add some logs, better exception handling

--- matrix_admin_bot/commands/account_validity.py
+++ matrix_admin_bot/commands/account_validity.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import time
 from collections.abc import Mapping
@@ -5,6 +6,8 @@ from datetime import datetime, timedelta
 from typing import Any
 
 import aiofiles
+import structlog
+from aiohttp import ClientConnectionError
 from matrix_bot.bot import MatrixClient
 from matrix_bot.eventparser import MessageEventParser
 from nio import MatrixRoom, RoomMessage
@@ -13,6 +16,8 @@ from typing_extensions import override
 from matrix_command_bot.util import get_server_name
 from matrix_command_bot.validation import IValidator
 from matrix_command_bot.validation.simple_command import SimpleValidatedCommand
+
+logger = structlog.getLogger(__name__)
 
 
 class AccountValidityCommand(SimpleValidatedCommand):
@@ -35,9 +40,12 @@ class AccountValidityCommand(SimpleValidatedCommand):
         event_parser.do_not_accept_own_message()
         self.user_ids = event_parser.command(self.KEYWORD).split()
 
-        self.failed_user_ids: list[str] = []
-
-        self.json_report: dict[str, Any] = {}
+        self.json_report: dict[str, Any] = {"command": self.KEYWORD}
+        self.json_report.setdefault("summary", {})
+        self.json_report["summary"].setdefault("success", 0)
+        self.json_report["summary"].setdefault("failed", 0)
+        self.json_report.setdefault("details", {})
+        self.json_report.setdefault("failed_users", "")
 
         self.server_name = get_server_name(self.matrix_client.user_id)
 
@@ -46,40 +54,82 @@ class AccountValidityCommand(SimpleValidatedCommand):
         if get_server_name(user_id) != self.server_name:
             return True
 
-        self.json_report.setdefault(user_id, {})
+        resp = None
+        for retry_nb in range(10):
+            try:
+                resp = await self.matrix_client.send(
+                    "POST",
+                    "/_synapse/admin/v1/account_validity/validity",
+                    headers={
+                        "Authorization": f"Bearer {self.matrix_client.access_token}"
+                    },
+                    data=json.dumps(
+                        {
+                            "user_id": user_id,
+                            "expiration_ts": expiration_ts,
+                            "enable_renewal_emails": "true",
+                        }
+                    ),
+                )
+                if resp.ok:
+                    break
+                # Let's also stop there if we get a client error that
+                # is not a rate limit.
+                if resp.status < 500 and resp.status != 429:
+                    break
+            except ClientConnectionError as e:
+                logger.warning("Bot Admin has lost connection for %s: %s", user_id, e)
 
-        resp = await self.matrix_client.send(
-            "POST",
-            "/_synapse/admin/v1/account_validity/validity",
-            headers={"Authorization": f"Bearer {self.matrix_client.access_token}"},
-            data=json.dumps(
-                {
-                    "user_id": user_id,
-                    "expiration_ts": expiration_ts,
-                    "enable_renewal_emails": "true",
-                }
-            ),
-        )
-        if not resp.ok:
+            # use some backoff
+            await asyncio.sleep(0.5 * retry_nb)
+
+        if resp and resp.ok:
             json_body = await resp.json()
-            self.json_report[user_id].update(json_body)
-            self.failed_user_ids.append(user_id)
-            return False
+            logger.info("Account Validity Done for %s: %s", user_id, str(json_body))
+            self.json_report["summary"]["success"] = (
+                self.json_report["summary"]["success"] + 1
+            )
 
+        elif resp:
+            json_body = await resp.json()
+            logger.info("Account Validity Done for %s: %s", user_id, str(json_body))
+            self.json_report["summary"]["failed"] = (
+                self.json_report["summary"]["failed"] + 1
+            )
+            self.json_report["failed_users"] = (
+                self.json_report["failed_users"] + user_id + " "
+            )
+        else:
+            error_message = (
+                "No response from /_synapse/admin/v1/account_validity/validity"
+            )
+            logger.info("Account Validity failed for %s: %s", user_id, error_message)
+            self.json_report["summary"]["failed"] = (
+                self.json_report["summary"]["failed"] + 1
+            )
+            self.json_report["failed_users"] = (
+                self.json_report["failed_users"] + user_id + " "
+            )
         return True
 
     @override
     async def simple_execute(self) -> bool:
-        now_plus_6months_datetime = datetime.now() + timedelta(days=180)  # noqa: DTZ005
-        now_plus_6months = int(round(now_plus_6months_datetime.timestamp() * 1000))
-        for user_id in self.user_ids:
-            await self.account_validity(user_id, now_plus_6months)
+        users = self.get_users()
+        result = len(users) > 0
+        if result:
+            now_plus_6months_datetime = datetime.now() + timedelta(days=180)  # noqa: DTZ005
+            now_plus_6months = int(round(now_plus_6months_datetime.timestamp() * 1000))
+            for user_id in users:
+                result = result and await self.account_validity(
+                    user_id, now_plus_6months
+                )
+        else:
+            self.json_report["summary"]["status"] = "FAILED"
+            self.json_report["summary"]["reason"] = "There is no notice to send"
 
-        if self.failed_user_ids:
-            self.json_report["command"] = self.KEYWORD
+        if self.json_report and result:
             await self.send_report()
-
-        return not self.failed_user_ids
+        return result
 
     @property
     @override
@@ -93,6 +143,7 @@ class AccountValidityCommand(SimpleValidatedCommand):
         )
 
     async def send_report(self) -> None:
+        logger.info("result=%s", self.json_report)
         async with aiofiles.tempfile.NamedTemporaryFile(suffix=".json") as tmpfile:
             await tmpfile.write(
                 json.dumps(self.json_report, indent=2, sort_keys=True).encode()
@@ -107,17 +158,9 @@ class AccountValidityCommand(SimpleValidatedCommand):
                 thread_root=self.message.event_id,
             )
 
-        if self.failed_user_ids:
-            text = "\n".join(
-                [
-                    "Couldn't set the account validity of the following users:",
-                    "",
-                    *[f"- {user_id}" for user_id in self.failed_user_ids],
-                ]
-            )
-            await self.matrix_client.send_markdown_message(
-                self.room.room_id,
-                text,
-                reply_to=self.message.event_id,
-                thread_root=self.message.event_id,
-            )
+    def get_users(self) -> set[str]:
+        users: set[str] = set()
+        for user_id in self.user_ids:
+            if user_id.startswith("@") and get_server_name(user_id) == self.server_name:
+                users.add(user_id)
+        return users
--- tests/matrix_admin_bot/commands/test_account_validity.py
+++ tests/matrix_admin_bot/commands/test_account_validity.py
@@ -22,6 +22,9 @@ async def test_account_validity() -> None:
         room, USER1_ID, "!account_validity @user_to_reset:example.org"
     )
 
+    # send the report a result
+    mocked_client.send_file_message.assert_awaited_once()
+    mocked_client.send_file_message.reset_mock()
     #  one call to set the account validity
     assert len(mocked_client.send.await_args_list) == 1
     assert "/account_validity/" in mocked_client.send.await_args_list[0][0][1]
@@ -31,12 +34,12 @@ async def test_account_validity() -> None:
 
 
 @pytest.mark.asyncio()
-async def test_failed_account_validity() -> None:
+async def test_failed_account_validity_with_error_500() -> None:
     mocked_client, t = await create_fake_command_bot(
         [AccountValidityCommand], secure_validator=OkValidator()
     )
     mocked_client.send = AsyncMock(
-        return_value=Mock(ok=False, json=AsyncMock(return_value={}))
+        return_value=Mock(ok=False, json=AsyncMock(return_value={}), status=500)
     )
 
     room = MatrixRoom("!roomid:example.org", USER1_ID)
@@ -45,6 +48,37 @@ async def test_failed_account_validity() -> None:
         room, USER1_ID, "!account_validity @user_to_reset:example.org"
     )
 
-    mocked_client.check_sent_message("Couldn't set the account validity")
+    # send the report a result
+    mocked_client.send_file_message.assert_awaited_once()
+    mocked_client.send_file_message.reset_mock()
+    # no call to fetch the users, and one call to send the notice directly to the user
+    assert len(mocked_client.send.await_args_list) == 10
+    assert "/account_validity/validity" in mocked_client.send.await_args_list[0][0][1]
+    mocked_client.send.reset_mock()
+
+    t.cancel()
+
+
+@pytest.mark.asyncio()
+async def test_failed_account_validity_on_other_instance() -> None:
+    mocked_client, t = await create_fake_command_bot(
+        [AccountValidityCommand], secure_validator=OkValidator()
+    )
+    mocked_client.send = AsyncMock(
+        return_value=Mock(ok=True, json=AsyncMock(return_value={}))
+    )
+
+    room = MatrixRoom("!roomid:example.org", USER1_ID)
+
+    await mocked_client.fake_synced_text_message(
+        room, USER1_ID, "!account_validity @user_to_reset:anotherexample.org"
+    )
+
+    # send the report a result
+    mocked_client.send_file_message.assert_not_awaited()
+    mocked_client.send_file_message.reset_mock()
+    # no call to fetch the users, and one call to send the notice directly to the user
+    assert len(mocked_client.send.await_args_list) == 0
+    mocked_client.send.reset_mock()
 
     t.cancel()
