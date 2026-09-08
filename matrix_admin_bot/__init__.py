af341c6ff3ad353066b0052d2d37f2faad08ac87
Mathieu Velten <mathieu.velten@beta.gouv.fr>
2026-09-09 00:12:05 +0200

    Add support of targeting domain emails

--- matrix_admin_bot/__init__.py
+++ matrix_admin_bot/__init__.py
@@ -6,6 +6,7 @@ from matrix_bot.client import MatrixClient
 from matrix_bot.eventparser import MessageEventParser
 from nio import MatrixRoom, RoomMessage
 
+from matrix_admin_bot.admin_client import AdminClient
 from matrix_command_bot.command import ICommand
 from matrix_command_bot.util import get_server_name, is_local_user, send_report
 from matrix_command_bot.validation.simple_command import SimpleValidatedCommand
@@ -23,6 +24,7 @@ class InteractiveValidatedCommand(SimpleValidatedCommand):
         extra_config: Mapping[str, Any],
     ) -> None:
         super().__init__(room, message, matrix_client, extra_config)
+        self.admin_client: AdminClient = extra_config.get("admin_client")  # pyright: ignore[reportAttributeAccessIssue]
 
         self.keyword = keyword
 
@@ -79,6 +81,11 @@ class UserRelatedCommand(InteractiveValidatedCommand):
         extra_config: Mapping[str, Any],
     ) -> None:
         super().__init__(room, message, matrix_client, keyword, extra_config)
+        server_name = get_server_name(self.matrix_client.user_id)
+        assert server_name  # noqa: S101
+        self.server_name: str = server_name
+        self.user_ids: list[str] = []
+        self.mxid_to_emails: dict[str, list[str]] = {}
 
         self.transform_cmd_input_fct: (
             Callable[[type[ICommand], list[str]], Awaitable[list[str]]] | None
@@ -86,12 +93,48 @@ class UserRelatedCommand(InteractiveValidatedCommand):
 
     @override
     async def should_execute(self) -> bool:
-        self.user_ids = self.command_text.split()
-
-        if self.transform_cmd_input_fct:
-            self.user_ids = await self.transform_cmd_input_fct(
-                self.__class__, self.user_ids
-            )
+        await self.get_user_ids_from_args(self.command_text.split())
         return any(
             is_local_user(user_id, self.server_name) for user_id in self.user_ids
         )
+
+    # TODO reduce complexity
+    async def get_user_ids_from_args(self, args: list[str]) -> None:  # noqa: C901
+        email_args: list[str] = []
+        domains: set[str] = set()
+        all_local_users = False
+        for arg in args:
+            if arg in ["all", self.server_name]:
+                all_local_users = True
+                break
+
+            if len(arg) > 0 and arg[0] == "@" and ":" in arg:
+                self.user_ids.append(arg)
+            elif "@" in arg:
+                email_args.append(arg)
+            else:
+                domains.add(arg)
+
+        # TODO use sydent /info to check if a domain is this server responsability
+
+        if all_local_users or domains:
+            mas_user_id_to_emails: dict[str, list[str]] = {}
+            mas_user_emails = await self.admin_client.get_user_emails()
+
+            for email, mas_id in mas_user_emails.items():
+                domain = email.split("@")[1]
+                if all_local_users or domain in domains:
+                    mas_user_id_to_emails.setdefault(mas_id, []).append(email)
+
+            for mas_user_id, emails in mas_user_id_to_emails.items():
+                user_id = await self.admin_client.get_user(
+                    self.server_name, mas_user_id
+                )
+                if user_id:
+                    self.user_ids.append(user_id)
+                    self.mxid_to_emails[user_id] = emails
+
+        if self.transform_cmd_input_fct:
+            self.user_ids.extend(
+                await self.transform_cmd_input_fct(self.__class__, email_args)
+            )
--- matrix_admin_bot/commands/next/room_state_v2.py
+++ matrix_admin_bot/commands/next/room_state_v2.py
@@ -5,7 +5,6 @@ from matrix_bot.bot import MatrixClient
 from nio import MatrixRoom, RoomMessage
 
 from matrix_admin_bot import InteractiveValidatedCommand
-from matrix_admin_bot.admin_client import AdminClient
 from matrix_command_bot.util import get_server_name
 
 
@@ -20,8 +19,6 @@ class RoomStateCommandV2(InteractiveValidatedCommand):
         extra_config: Mapping[str, Any],
     ) -> None:
         super().__init__(room, message, matrix_client, self.KEYWORD, extra_config)
-        self.admin_client: AdminClient = extra_config.get("admin_client")  # pyright: ignore[reportAttributeAccessIssue]
-
         self.failed_room_ids: list[str] = []
 
     async def room_details(self, room_id: str) -> bool:
